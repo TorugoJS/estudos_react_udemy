6d94653914ceb93e0c99fd52b7dfb378e2dfd144
TorugoJS <victor.devfrontend@gmail.com>
2022-07-29 20:05:38 -0300

    estudos_react

--- src/components/FirstComponent.js
+++ src/components/FirstComponent.js
@@ -1,10 +1,22 @@
+//arquivo js
 
 const FirstComponent = () => {
+
+    //função
+
+
+    /*
+    multipla linhas
+    */
+
     return (
+
         <div>
+            {/*Algum comentário */}
             <h1>Primeiro</h1>
             <p className="teste">Meu texto</p>
         </div>
+
     )
 }
 
