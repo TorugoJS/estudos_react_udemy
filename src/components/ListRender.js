133547411240fa552f7a2db82786973509387c84
TorugoJS <victor.devfrontend@gmail.com>
2022-08-01 20:17:55 -0300

    estudos_react

--- src/components/ListRender.js
+++ src/components/ListRender.js
@@ -4,14 +4,27 @@ const ListRender = () => {
 
     const [list] = useState(["Victor", "Hugo", "Pina", "Reis"])
 
-    const [users] = useState([
+
+    const [users, setUsers] = useState([
         { id: 1, name: "João", age: 18 },
-        { id: 56455, name: "Maria", age: 60 },
-        { id: 897899, name: "Joana", age: 45 }
+        { id: 2, name: "Maria", age: 60 },
+        { id: 3, name: "Joana", age: 45 }
     ]);
+
+
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4);
+
+        setUsers((prevUsers) => {
+            console.log(prevUsers)
+            return prevUsers.filter((user) => randomNumber !== user.id)
+
+        })
+    }
 
     return (
         <div>
+
             <ul>
                 {list.map((item, i) => (
                     <li key={i}>{item}</li>
@@ -24,6 +37,9 @@ const ListRender = () => {
                 ))}
 
             </ul>
+
+            <button onClick={deleteRandom}>Delete random user</button>
+
         </div>
     )
 }
