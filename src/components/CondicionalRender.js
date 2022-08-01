5fc46999c791a842ab571514b0ba8c42ebe96d1c
TorugoJS <victor.devfrontend@gmail.com>
2022-08-01 20:53:00 -0300

    estudos_react

--- src/components/CondicionalRender.js
+++ src/components/CondicionalRender.js
@@ -1,16 +1,32 @@
 import { useState } from "react";
 
 const CondicionalRender = () => {
-
     const [x] = useState(false)
 
-  return (
-    <div>
-        <h1>Isso será exibido?</h1>
-        {x && <p>Se x for true, sim !</p>}
-        {!x && <p>Agora x é falso!</p>}
-    </div>
-  )
+    const [name, setName] = useState("Victor")
+
+    return (
+        <div>
+            <h1>Isso será exibido?</h1>
+            {x && <p>Se x for true, sim !</p>}
+            {!x && <p>Agora x é falso!</p>}
+
+            <h1>If ternário</h1>
+            {name === "Victor" ? (
+                <div>
+                    <p>O nome é Victor</p>
+                </div>
+
+            ) : (
+
+                <div>
+                    <p>O nome não é Victor</p>
+                </div>
+            )}
+            <button onClick={()=> setName("Pina")}>Mudar nome</button>
+
+        </div>
+    )
 }
 
 export default CondicionalRender;
