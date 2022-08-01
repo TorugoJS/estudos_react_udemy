0603700bddf0ba8761fea2ce1bca21fadef7dc72
TorugoJS <victor.devfrontend@gmail.com>
2022-08-01 20:06:06 -0300

    estudos_react

--- src/components/ListRender.js
+++ src/components/ListRender.js
@@ -4,12 +4,25 @@ const ListRender = () => {
 
     const [list] = useState(["Victor", "Hugo", "Pina", "Reis"])
 
+    const [users] = useState([
+        { id: 1, name: "João", age: 18 },
+        { id: 56455, name: "Maria", age: 60 },
+        { id: 897899, name: "Joana", age: 45 }
+    ]);
+
     return (
         <div>
             <ul>
-                {list.map((item) => (
-                    <li>{item}</li>
+                {list.map((item, i) => (
+                    <li key={i}>{item}</li>
                 ))}
+            </ul>
+
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name} - {user.age}</li>
+                ))}
+
             </ul>
         </div>
     )
